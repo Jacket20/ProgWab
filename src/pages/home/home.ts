1161d114a2c48b54970a1445f9ba708cab989f8b
Jeremy <masson.jeremy73000@gmail.com>
2018-03-14 15:46:14 +0100

    searchbarre

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -6,9 +6,37 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  Results:fakeResult;
   constructor(public navCtrl: NavController) {
+    //this.Results = fakeResult;
+    this.Results = [];
+  }
+  items: string[];
 
+  initializeItems() {
+    this.items = this.Results.title;
+    
   }
 
+  getItems(ev: any) {
+    let val = ev.target.value;
+    if(val == ''){
+      this.Results = [];
+    }else{
+      this.Results = fakeResult;
+    }
+  }
 }
+
+export interface Result{
+  title: string;
+  author: string;
+  date: string;
+  image: string;
+}
+
+const fakeResult: Result[] = [
+  {title:'La Guerre des étoiles', author:'Geoges Lucas',date:'1977',image:'https://image.tmdb.org/t/p/w600_and_h900_bestv2/yVaQ34IvVDAZAWxScNdeIkaepDq.jpg'},
+  {title:'Les 8 Salopards', author:'Quentin Tarantino',date:'2015',image:'https://image.tmdb.org/t/p/w600_and_h900_bestv2/sRq90sQq7S68wWnAk9eBxFAvWVz.jpg'},
+  {title:'Django', author:'tarantino',date:'2012',image:'https://image.tmdb.org/t/p/w600_and_h900_bestv2/iWXGPkf8TwRuXecfDs0j3dDBNLA.jpg'}
+];
